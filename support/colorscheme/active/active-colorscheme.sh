370b173dbbb31821e028a92e4b64f641fa8d83b9
kevin <kevin@slashspaces.com>
2026-04-01 10:53:02 +0800

    update confg

--- support/colorscheme/active/active-colorscheme.sh
+++ support/colorscheme/active/active-colorscheme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Filename: ~/dotfiles/colorscheme/list/catppuccin-mocha.sh
-# ~/dotfiles/colorscheme/list/catppuccin-mocha.sh
+# Filename: ~/dotfiles/colorscheme/list/catppuccin-macchiato.sh
+# ~/dotfiles/colorscheme/list/catppuccin-macchiato.sh
 
 # Lighter markdown headings
 # 4 colors to the right for these ligher headings
@@ -12,74 +12,65 @@
 # C (#37f499) becomes when darkened 4 steps to the right.
 #
 # Markdown heading 1 - color04
-linkarzu_color18="#796394"
+linkarzu_color18="#765f93"
 # Markdown heading 2 - color02
-linkarzu_color19="#526c96"
+linkarzu_color19="#536892"
 # Markdown heading 3 - color03
-linkarzu_color20="#648861"
+linkarzu_color20="#648359"
 # Markdown heading 4 - color01
-linkarzu_color21="#8d6067"
+linkarzu_color21="#8e5c60"
 # Markdown heading 5 - color05
-linkarzu_color22="#6c7298"
+linkarzu_color22="#6d7195"
 # Markdown heading 6 - color08
-linkarzu_color23="#966b51"
+linkarzu_color23="#93654c"
 # Markdown heading foreground
 # usually set to color10 which is the terminal background
-linkarzu_color26=#1e1e2e
+linkarzu_color26=#24273a
 
 # Mauve
-linkarzu_color04="#cba6f7"
+linkarzu_color04="#c6a0f6"
 # Blue
-linkarzu_color02="#89b4fa"
+linkarzu_color02="#8aadf4"
 # Green
-linkarzu_color03="#a6e3a1"
+linkarzu_color03="#a6da95"
 # Maroon
-linkarzu_color01="#eba0ac"
+linkarzu_color01="#ee99a0"
 # Lavender
-linkarzu_color05="#b4befe"
+linkarzu_color05="#b7bdf8"
 # Peach
-linkarzu_color08="#fab387"
+linkarzu_color08="#f5a97f"
 # Sapphire
-linkarzu_color06="#74c7ec"
+linkarzu_color06="#7dc4e4"
 # Surface 0
-# Markdown codeblock
-linkarzu_color07="#313244"
+# Markdown codeblock, lualine color across
+linkarzu_color07="#363a4f"
 # Subtext0
 # Comments
-linkarzu_color09="#a6adc8"
+linkarzu_color09="#a5adcb"
 # Base
 # Terminal and neovim background
-linkarzu_color10="#1e1e2e"
+linkarzu_color10="#24273a"
 # Red
 # Underline spellbad
-linkarzu_color11="#f38ba8"
+linkarzu_color11="#ed8796"
 # Yellow
 # Underline spellcap
-linkarzu_color12="#f9e2af"
+linkarzu_color12="#eed49f"
 # Surface2
 # line across cursor
-linkarzu_color13="#585b70"
+linkarzu_color13="#5b6078"
 # Text
 # Cursor and tmux windows text
-linkarzu_color14="#cdd6f4"
+linkarzu_color14="#cad3f5"
 # Surface1
-# Tmux inactive windows
-linkarzu_color15="#314430"
+# Tmux inactive windows (7 colors to the right)
+linkarzu_color15="#31412c"
 # Flamingo
 # Selected text
-linkarzu_color16="#f2cdcd"
+linkarzu_color16="#f0c6c6"
 # Lualine across
-linkarzu_color17="#313244"
+linkarzu_color17="#363a4f"
 # Cursor color
 linkarzu_color24="#F712FF"
-# https://www.colorhexa.com/1e1e2e
-# #0f0f16
-# #161622
-# #1e1e2e
-# #26263a
-# #2d2d46
-# #353552
-# #3d3d5d
-# #454569
 # Background of inactive tmux pane
 linkarzu_color25=#232e3b
