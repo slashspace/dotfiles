ac180215c6ca18f03278d7316cb89de9a90f7176
dingsheng <dingsheng1214@gmail.com>
2026-04-15 23:05:16 +0800

    claude init

--- system/lib/modules/vpn.sh
+++
@@ -1,7 +0,0 @@
-
-# 1. 建立隧道的快捷命令
-alias vpn-bridge="ssh -f -D 1080 -N -p 2222 dingsheng@127.0.0.1"“ssh -f -D 1080 -N -p 2222 dingsheng@127.0.0.1”
-
-# 2. 开启/关闭终端代理
-alias vpn-on="export ALL_PROXY=socks5://127.0.0.1:1080"
-alias vpn-off="unset ALL_PROXY"
