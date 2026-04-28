67908f8b7614c878cc4a19efd9a73d5116e8e082
dingsheng <dingsheng1214@gmail.com>
2026-04-28 17:55:13 +0800

    chore: delete experimental sketchybar widgets (memory, cpu, front_app, mic, media, timer)

--- modules/sketchybar/plugins/front_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [ "$SENDER" = "front_app_switched" ]; then
-  # if [ "$INFO" = "kitty" ]; then
-  #   exit 0
-  # fi
-  sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO"
-fi
--- modules/sketchybar/plugins/zen.sh
+++ modules/sketchybar/plugins/zen.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-
 zen_on() {
   sketchybar --set apple.logo drawing=on \
-    --set '/cpu.*/' drawing=on \
     --set calendar icon.drawing=on \
     --set battery drawing=on \
-    --set mic drawing=on \
-    --set front_app drawing=on label.drawing=off \
     --set brew drawing=off \
     --set volume_icon drawing=off \
     --set spotify.anchor drawing=off \
     --set spotify.play updates=off \
-    --set timer drawing=off \
     --set volume drawing=off \
     --set github.bell drawing=off \
     --set wifi drawing=off
@@ -20,15 +15,11 @@ zen_on() {
 
 zen_off() {
   sketchybar --set apple.logo drawing=on \
-    --set '/cpu.*/' drawing=on \
     --set calendar icon.drawing=on \
     --set battery drawing=on \
-    --set mic drawing=on \
-    --set front_app drawing=on label.drawing=on \
     --set brew drawing=on \
     --set volume_icon drawing=on \
     --set spotify.play updates=on \
-    --set timer drawing=on \
     --set volume drawing=on \
     --set github.bell drawing=on \
     --set wifi drawing=on
